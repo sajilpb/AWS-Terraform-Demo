28a14b6b10b8b67aa1b542c059e33fc475b04e22
sajil <sajil>
2025-12-05 07:12:06 +0530

    upfates

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -4,11 +4,11 @@ variable "instance_type" {
 }
 
 variable "vpc_id" {
-    description = "The ID of the VPC where the EC2 instance will be deployed"
-    type        = string
+  description = "The ID of the VPC where the EC2 instance will be deployed"
+  type        = string
 }
 
 variable "vpc_subnets" {
-    description = "List of subnet IDs in the VPC"
-    type        = list(string)
+  description = "List of subnet IDs in the VPC"
+  type        = list(string)
 }
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,13 +6,13 @@ module "vpc" {
 
   azs             = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-  private_subnets  = ["10.0.103.0/24", "10.0.104.0/24"]
+  private_subnets = ["10.0.103.0/24", "10.0.104.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "Demo"
   }
 }
